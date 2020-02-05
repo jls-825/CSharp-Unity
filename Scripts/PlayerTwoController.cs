1b9ee06526ab25a5fa07ccb366b59ad59ae69f6a
Jeanna Shellenberger <44857184+jls-825@users.noreply.github.com>
2020-02-05 12:16:31 -0500

    Add files via upload

---
+++ Scripts/PlayerTwoController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoController : MonoBehaviour
+{
+    public float playerSpeed;
+    private float move;
+    public float jumpSpeed;
+    private bool isJumping;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal1");
+
+        rigidBody.velocity = new Vector2(move * playerSpeed, rigidBody.velocity.y);
+
+        if (move < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (move > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetButtonDown("Jump1") && !isJumping)
+        {
+            rigidBody.AddForce(new Vector2(rigidBody.velocity.x, jumpSpeed));
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
